e25841861e7fc7c285eb4434bf19e01cc904cab9
daimonz01 <44843350+daimonz01@users.noreply.github.com>
2019-01-12 09:53:24 +0700

    Update Callpost.js

--- Callpost.js
+++ Callpost.js
@@ -28,7 +28,8 @@ if(x=="2"){xxx="/2018/11/Post2.html"}
 if(x=="3"){xxx="/2018/11/Post3.html"}
 if(x=="4"){xxx="/2018/11/Post4.html"}
 if(x=="5"){xxx="/2018/11/Post5.html"}
-if(x=="6"){xxx="/2018/11/Post6.html"}a_to_ve[i].href="https://aman-dn.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="6"){xxx="/2018/11/Post6.html"}
+if(x=="7"){xxx="/2018/11/Post7.html"}a_to_ve[i].href="https://aman-dn.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
